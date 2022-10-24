a8eab1378ce091d2f3f2a1e37dc5f3af87ca506a
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-24 10:48:46 -0700

    Add event listener for keydown, trigger removal of h1, addition of new h1 and addition of p element
    Co-authored-by: Auston Moos <Austonhmoos2004@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
-// When page loads, run these functions
+// When keyboard is pressed, do this:
+document.addEventListener('keydown', (event) => {
 
+  // Remove old title
+  const oldTitle = document.querySelectorAll("h1")[0];
+  oldTitle.remove();
 
-// Remove old title
-const oldTitle = document.querySelectorAll("h1")[0];
-oldTitle.remove();
+  // Add new title
+  // Step 1: store element in variable
+  const h1Element = document.createElement("h1");
+  // Step 2: add text to element
+  h1Element.append("Webpage Recreation Practice");
+
+  // Add variable for HTML body
+  // Step 1: store container element in variable
+  const pageBody = document.querySelector("body");
+  // Step 2: add child element to container
+  pageBody.append(h1Element);
+
+  // Add paragraph
+  const newParagraph = document.createElement("p");
+  newParagraph.append("The HTML of this webpage was created with JavaScript.");
+  pageBody.append(newParagraph);
+  
+});
